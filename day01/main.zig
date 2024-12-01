9013d9e5abd5bc82f2f15dc026b0e5e191b49699
LilWeavs <andrewjw827@gmail.com>
2024-12-01 08:33:10 -0600

    day01

--- day01/main.zig
+++ day01/main.zig
@@ -1,97 +1,101 @@
 ﻿const std = @import("std");
 
+const ReturnType = struct {
+    answer: isize,
+    time: usize,
+};
+
 pub fn main() !void {
-    const part1 = solvePart1(@embedFile("input.txt"));
-    std.debug.print("Day 01|1: {d}\n", .{part1});
-    const part2 = solvePart2(@embedFile("input.txt"));
-    std.debug.print("Day 01|2: {d}\n", .{part2});
+    const part1 = try solvePart1(@embedFile("input.txt"));
+    std.debug.print("Day 01|1: {d} dt: {}\n", .{ part1.answer, part1.time });
+    const part2 = try solvePart2(@embedFile("input.txt"));
+    std.debug.print("Day 01|2: {d} dt: {}\n", .{ part2.answer, part2.time });
 }
 
-const numbers = [_][]const u8{ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-
-fn checkIfNumber(str: []const u8) usize {
-    for (str, 0..) |c, i| {
-        if (std.ascii.isDigit(c)) {
-            return (c - '0') * 10;
-        }
-
-        for (numbers, 1..) |numStr, num| {
-            for (0..numStr.len) |j| {
-                if (str[i + j] != numStr[j]) {
-                    break;
-                }
-            } else {
-                return num * 10;
-            }
-        }
-    }
-    return 0;
-}
-
-fn rcheckIfNumber(str: []const u8) usize {
-    for (0..str.len) |i| {
-        const c = str[str.len - 1 - i];
-        if (std.ascii.isDigit(c)) {
-            return (c - '0');
-        }
-
-        for (numbers, 1..) |numStr, num| {
-            if (i < numStr.len - 1) {
-                continue;
-            }
-            for (0..numStr.len) |j| {
-                if (str[str.len - 1 - i + j] != numStr[j]) {
-                    break;
-                }
-            } else {
-                return num;
-            }
-        }
-    }
-    return 0;
-}
-
-fn solvePart1(input: []const u8) usize {
+fn solvePart1(input: []const u8) !ReturnType {
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
 
-    var total: usize = 0;
-    while (lines.next()) |line| {
-        for (line) |c| {
-            if (std.ascii.isDigit(c)) {
-                total += (c - '0') * 10;
-                break;
-            }
-        }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-        for (0..line.len) |i| {
-            const c = line[line.len - 1 - i];
-            if (std.ascii.isDigit(c)) {
-                total += (c - '0');
-                break;
-            }
-        }
+    var left_list = std.ArrayList(isize).init(allocator);
+    defer left_list.deinit();
+    var right_list = std.ArrayList(isize).init(allocator);
+    defer right_list.deinit();
+
+    while (lines.next()) |line| {
+        var token = std.mem.tokenizeScalar(u8, line, ' ');
+
+        const left_num = try std.fmt.parseInt(isize, token.next().?, 10);
+        try left_list.append(left_num);
+
+        const right_num = try std.fmt.parseInt(isize, token.next().?, 10);
+        try right_list.append(right_num);
     }
 
-    return total;
+    var timer = try std.time.Timer.start();
+
+    std.mem.sort(isize, left_list.items, {}, std.sort.asc(isize));
+    std.mem.sort(isize, right_list.items, {}, std.sort.asc(isize));
+
+    var total: isize = 0;
+    for (left_list.items, right_list.items) |a, b| {
+        total += @intCast(@abs(b - a));
+    }
+
+    return .{ .answer = total, .time = timer.lap() };
 }
 
-fn solvePart2(input: []const u8) usize {
+fn solvePart2(input: []const u8) !ReturnType {
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
 
-    var total: usize = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var left_list = std.ArrayList(isize).init(allocator);
+    defer left_list.deinit();
+    var right_list = std.ArrayList(isize).init(allocator);
+    defer right_list.deinit();
+
     while (lines.next()) |line| {
-        total += checkIfNumber(line);
-        total += rcheckIfNumber(line);
+        var token = std.mem.tokenizeScalar(u8, line, ' ');
+
+        const left_num = try std.fmt.parseInt(isize, token.next().?, 10);
+        try left_list.append(left_num);
+
+        const right_num = try std.fmt.parseInt(isize, token.next().?, 10);
+        try right_list.append(right_num);
     }
-    return total;
+
+    var timer = try std.time.Timer.start();
+
+    std.mem.sort(isize, left_list.items, {}, std.sort.asc(isize));
+    std.mem.sort(isize, right_list.items, {}, std.sort.asc(isize));
+
+    var total: isize = 0;
+    var offset: usize = 0;
+    for (left_list.items) |item| {
+        for (right_list.items[offset..], 0..) |b, i| {
+            if (item == b) {
+                total += item;
+            } else if (b > item) {
+                offset += i;
+                break;
+            } else {}
+        }
+    }
+
+    return .{ .answer = total, .time = timer.lap() };
 }
 
-test "test-part1" {
-    const result = solvePart1(@embedFile("test1.txt"));
-    try std.testing.expectEqual(result, 142);
-}
+// test "test-part1" {
+//     const result = try solvePart1(@embedFile("test.txt"));
+//     try std.testing.expectEqual(result.answer, 11);
+// }
 
-test "test-part2" {
-    const result = solvePart2(@embedFile("test2.txt"));
-    try std.testing.expectEqual(result, 281);
-}
+// test "test-part2" {
+//     const result = solvePart2(@embedFile("test.txt"));
+//     // try std.testing.expectEqual(result.answer, 31);
+// }
