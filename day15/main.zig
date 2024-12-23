02db5f118c3eb6c72978f29c60d08c9778527e44
saltymrbighead <35853304+saltymrbighead@users.noreply.github.com>
2024-12-22 20:21:01 -0600

    day15 part 2

--- day15/main.zig
+++ day15/main.zig
@@ -8,8 +8,8 @@ const ReturnType = struct {
 pub fn main() !void {
     const part1 = try solvePart1(@embedFile("input.txt"));
     std.debug.print("Day 15|1: {d} dt: {d:6.2}ms\n", .{ part1.answer, @as(f32, @floatFromInt(part1.time)) / 1e6 });
-    // const part2 = try solvePart2(@embedFile("input.txt"), 101, 103);
-    // std.debug.print("Day 15|2: {d} dt: {d:6.2}ms\n", .{ part2.answer, @as(f32, @floatFromInt(part2.time)) / 1e6 });
+    const part2 = try solvePart2(@embedFile("input.txt"));
+    std.debug.print("Day 15|2: {d} dt: {d:6.2}ms\n", .{ part2.answer, @as(f32, @floatFromInt(part2.time)) / 1e6 });
 }
 
 // fn parseInput(input: []const u8, allocator: std.mem.Allocator) !std.ArrayList(Robot) {
@@ -31,6 +31,13 @@ pub fn main() !void {
 // }
 
 const Point = struct { x: usize = 0, y: usize = 0 };
+
+const Direction = enum {
+    Up,
+    Right,
+    Down,
+    Left,
+};
 
 fn solvePart1(input: []const u8) !ReturnType {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -71,97 +78,32 @@ fn solvePart1(input: []const u8) !ReturnType {
 
     var timer = try std.time.Timer.start();
 
-    var map = mapp.items;
-    outer: for (instructions.items) |instruction| {
+    const map = mapp.items;
+
+    for (instructions.items) |instruction| {
         // printMap(map);
-        if (instruction == '>') {
-            const nloc = Point{ .x = robot.x + 1, .y = robot.y };
-            const lf = map[nloc.y][nloc.x];
-            if (lf == '#') continue;
-
-            if (lf == 'O') {
-                for (robot.x + 2..map[nloc.y].len) |i| {
-                    if (map[nloc.y][i] == '.') {
-                        for (robot.x..i + 1) |j| {
-                            map[robot.y][j] = 'O';
-                        }
-                        map[nloc.y][nloc.x] = '@';
-                        map[robot.y][robot.x] = '.';
-                        break;
-                    } else if (map[robot.y][i] == '#') {
-                        continue :outer;
-                    }
+        switch (instruction) {
+            '^' => {
+                if (moveBox(map, robot, .Up)) {
+                    robot.y -= 1;
                 }
-            } else {
-                std.mem.swap(u8, &map[robot.y][robot.x], &map[nloc.y][nloc.x]);
-            }
-            robot = nloc;
-        } else if (instruction == '<') {
-            const nloc = Point{ .x = robot.x - 1, .y = robot.y };
-            const lf = map[nloc.y][nloc.x];
-            if (lf == '#') continue;
-
-            if (lf == 'O') {
-                for (0..robot.x + 1) |i| {
-                    if (map[nloc.y][robot.x - i] == '.') {
-                        for (0..i + 1) |j| {
-                            map[robot.y][robot.x - j] = 'O';
-                        }
-                        map[nloc.y][nloc.x] = '@';
-                        map[robot.y][robot.x] = '.';
-                        break;
-                    } else if (map[robot.y][robot.x - i] == '#') {
-                        continue :outer;
-                    }
+            },
+            '>' => {
+                if (moveBox(map, robot, .Right)) {
+                    robot.x += 1;
                 }
-            } else {
-                std.mem.swap(u8, &map[robot.y][robot.x], &map[nloc.y][nloc.x]);
-            }
-            robot = nloc;
-        } else if (instruction == 'v') {
-            const nloc = Point{ .x = robot.x, .y = robot.y + 1 };
-            const lf = map[nloc.y][nloc.x];
-            if (lf == '#') continue;
-
-            if (lf == 'O') {
-                for (robot.y..map.len) |j| {
-                    if (map[j][robot.x] == '.') {
-                        for (robot.y..j + 1) |i| {
-                            map[i][robot.x] = 'O';
-                        }
-                        map[nloc.y][nloc.x] = '@';
-                        map[robot.y][robot.x] = '.';
-                        break;
-                    } else if (map[j][robot.x] == '#') {
-                        continue :outer;
-                    }
+            },
+            'v' => {
+                if (moveBox(map, robot, .Down)) {
+                    robot.y += 1;
                 }
-            } else {
-                std.mem.swap(u8, &map[robot.y][robot.x], &map[nloc.y][nloc.x]);
-            }
-            robot = nloc;
-        } else if (instruction == '^') {
-            const nloc = Point{ .x = robot.x, .y = robot.y - 1 };
-            const lf = map[nloc.y][nloc.x];
-            if (lf == '#') continue;
-
-            if (lf == 'O') {
-                for (0..robot.y + 1) |j| {
-                    if (map[robot.y - j][robot.x] == '.') {
-                        for (0..j + 1) |i| {
-                            map[robot.y - i][robot.x] = 'O';
-                        }
-                        map[nloc.y][nloc.x] = '@';
-                        map[robot.y][robot.x] = '.';
-                        break;
-                    } else if (map[robot.y - j][robot.x] == '#') {
-                        continue :outer;
-                    }
+            },
+            '<' => {
+                if (moveBox(map, robot, .Left)) {
+                    robot.x -= 1;
                 }
-            } else {
-                std.mem.swap(u8, &map[robot.y][robot.x], &map[nloc.y][nloc.x]);
-            }
-            robot = nloc;
+            },
+            else => unreachable,
         }
     }
 
@@ -174,9 +116,173 @@ fn solvePart1(input: []const u8) !ReturnType {
             }
         }
     }
-    printMap(map);
 
     return .{ .answer = @intCast(total), .time = timer.lap() };
+}
+
+fn solvePart2(input: []const u8) !ReturnType {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    var mapp = std.ArrayList([]u8).init(allocator);
+    defer {
+        for (mapp.items) |row| {
+            allocator.free(row);
+        }
+        mapp.deinit();
+    }
+
+    var robot = Point{};
+
+    var rows: usize = 0;
+    var cols: usize = 0;
+    while (lines.next()) |line| : (rows += 1) {
+        var tmp = std.ArrayList(u8).init(allocator);
+
+        for (line) |c| {
+            switch (c) {
+                '#' => try tmp.appendNTimes('#', 2),
+                '.' => try tmp.appendNTimes('.', 2),
+                '@' => {
+                    try tmp.appendSlice("@.");
+                    robot = Point{ .x = tmp.items.len - 2, .y = rows };
+                },
+                'O' => try tmp.appendSlice("[]"),
+                else => unreachable,
+            }
+        }
+        cols = tmp.items.len;
+        try mapp.append(try tmp.toOwnedSlice());
+        if (std.mem.indexOfAny(u8, lines.peek().?, "><^v")) |_| break;
+    }
+
+    var instructions = std.ArrayList(u8).init(allocator);
+    defer instructions.deinit();
+    while (lines.next()) |line| {
+        try instructions.appendSlice(line);
+    }
+
+    var timer = try std.time.Timer.start();
+
+    const map = mapp.items;
+    for (instructions.items) |instruction| {
+        // printMap(map);
+        switch (instruction) {
+            '^' => {
+                if (checkDoubleBox(map, robot, .Up)) {
+                    moveDoubleBox(map, robot, .Up);
+                    robot.y -= 1;
+                }
+            },
+            '>' => {
+                if (checkDoubleBox(map, robot, .Right)) {
+                    moveDoubleBox(map, robot, .Right);
+                    robot.x += 1;
+                }
+            },
+            'v' => {
+                if (checkDoubleBox(map, robot, .Down)) {
+                    moveDoubleBox(map, robot, .Down);
+                    robot.y += 1;
+                }
+            },
+            '<' => {
+                if (checkDoubleBox(map, robot, .Left)) {
+                    moveDoubleBox(map, robot, .Left);
+                    robot.x -= 1;
+                }
+            },
+            else => unreachable,
+        }
+    }
+
+    var total: usize = 0;
+
+    for (1..map.len) |j| {
+        for (1..cols) |i| {
+            if (map[j][i] == '[') {
+                total += 100 * j + i;
+            }
+        }
+    }
+
+    // printMap(map);
+
+    return .{ .answer = @intCast(total), .time = timer.lap() };
+}
+
+fn checkDoubleBox(map: [][]u8, cur_point: Point, direction: Direction) bool {
+    const new_point = getNewPoint(cur_point, direction);
+    const c = map[new_point.y][new_point.x];
+    if (direction == .Right or direction == .Left) {
+        return switch (c) {
+            '#' => false,
+            '[' => checkDoubleBox(map, new_point, direction),
+            ']' => checkDoubleBox(map, new_point, direction),
+            '.' => true,
+            else => unreachable,
+        };
+    } else {
+        return switch (c) {
+            '#' => false,
+            '[' => (checkDoubleBox(map, new_point, direction) and checkDoubleBox(map, getNewPoint(new_point, .Right), direction)),
+            ']' => (checkDoubleBox(map, new_point, direction) and checkDoubleBox(map, getNewPoint(new_point, .Left), direction)),
+            '.' => true,
+            else => unreachable,
+        };
+    }
+}
+
+fn moveDoubleBox(map: [][]u8, cur_point: Point, direction: Direction) void {
+    const new_point = getNewPoint(cur_point, direction);
+    const c = map[new_point.y][new_point.x];
+    if (direction == .Left or direction == .Right) {
+        if (c == '[' or c == ']') {
+            moveDoubleBox(map, new_point, direction);
+        }
+        std.mem.swap(u8, &map[new_point.y][new_point.x], &map[cur_point.y][cur_point.x]);
+    } else {
+        if (c == '[') {
+            moveDoubleBox(map, new_point, direction);
+            moveDoubleBox(map, getNewPoint(new_point, .Right), direction);
+        } else if (c == ']') {
+            _ = moveDoubleBox(map, new_point, direction);
+            _ = moveDoubleBox(map, getNewPoint(new_point, .Left), direction);
+        }
+
+        std.mem.swap(u8, &map[new_point.y][new_point.x], &map[cur_point.y][cur_point.x]);
+    }
+}
+
+fn getNewPoint(point: Point, direction: Direction) Point {
+    return switch (direction) {
+        .Up => Point{ .x = point.x, .y = point.y - 1 },
+        .Right => Point{ .x = point.x + 1, .y = point.y },
+        .Down => Point{ .x = point.x, .y = point.y + 1 },
+        .Left => Point{ .x = point.x - 1, .y = point.y },
+    };
+}
+
+fn moveBox(map: [][]u8, cur_point: Point, direction: Direction) bool {
+    const new_point = getNewPoint(cur_point, direction);
+
+    const c = map[new_point.y][new_point.x];
+    var swap = false;
+    if (c == '#') {
+        return false;
+    } else if (c == 'O') {
+        swap = moveBox(map, new_point, direction);
+    } else {
+        swap = true;
+    }
+
+    if (swap == true) {
+        std.mem.swap(u8, &map[new_point.y][new_point.x], &map[cur_point.y][cur_point.x]);
+        return true;
+    }
+    return false;
 }
 
 fn printMap(map: [][]u8) void {
@@ -187,6 +293,11 @@ fn printMap(map: [][]u8) void {
 }
 
 test "part1" {
-    const part1 = try solvePart1(@embedFile("test.txt"), 11, 7);
-    try std.testing.expectEqual(12, part1.answer);
+    const part1 = try solvePart1(@embedFile("test.txt"));
+    try std.testing.expectEqual(10092, part1.answer);
+}
+
+test "part2" {
+    const part1 = try solvePart2(@embedFile("test.txt"));
+    try std.testing.expectEqual(9021, part1.answer);
 }
